2a59f48c00bb48cde4ec90621de7e34c0c1081c2
psh686868 <psh686868@163.com>
2017-12-27 14:42:05 +0800

    🤪 添加配置文件

--- src/main/java/com/scms/managerserver/controller/DemoController.java
+++ src/main/java/com/scms/managerserver/controller/DemoController.java
@@ -3,6 +3,11 @@ package com.scms.managerserver.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * @author PSH
  * Date: 2017/12/27
@@ -11,8 +16,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    @GetMapping("/index")
-    public String index () {
-        return "hello every one";
+    @GetMapping("/dumpThreadInfo")
+    public List<ThreadInfo> index () {
+        List<ThreadInfo> threadInfos = Arrays.asList(ManagementFactory.getThreadMXBean().dumpAllThreads(true, true));
+
+        return threadInfos;
+    }
+
+    @GetMapping("/getThreadInfo")
+    public List<ThreadInfo> threadInfos () {
+        List<ThreadInfo> threadInfos = Arrays.asList(ManagementFactory.getThreadMXBean().getThreadInfo(new long[]{11192,13648,13590,501,13589,11737}));
+
+        return threadInfos;
     }
 }
